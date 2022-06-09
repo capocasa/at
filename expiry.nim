08da8e0e910a5617f89b55156a934bbee85bff8e
Carlo Capocasa <carlo@capocasa.net>
2022-06-09 14:41:45 +0200

    basically works

--- expiry.nim
+++ expiry.nim
@@ -2,48 +2,47 @@
 import asyncdispatch, asyncfutures, tables, times, limdb, flatty
 
 type
-  Expiry = object
-    renew: Duration
-    trigger: ref Future[void]
-    db: Database
-    timedb: Database
-    keydb: Database
+  Expiry* = object
+    renew*: Duration
+    trigger*: ref Future[void]
+    db*: Database
+    timedb*: Database
+    keydb*: Database
 
-proc next(e: Expiry): string=
+proc next*(e: Expiry): string=
   for tb in e.timedb.keys:
-    echo "NEXT KEY ", tb
-    result = tb
-    break
+    echo "NEXT T ", tb.fromFlatty(Time), " NOW ", getTime()
+    return tb
 
-proc process(e: Expiry) {.async.} =
-  var now:Time
-  now = getTime()
-  if e.timedb.len == 0:
-    discard await withTimeout[void](e.trigger[], e.renew.inMilliseconds.int)
-    e.trigger[] = newFuture[void]("expiry" & e.db.name)
-    asyncCheck e.process()
-    return
-  let tb = e.next
+proc remove(e: Expiry, tb: string) =
   let key = e.timedb[tb]
-  let t = tb.fromFlatty(Time)
-  echo "TB ", tb, " KEY ", key
-  if t <= now:
-    try:
-      e.db.del(key)
-    except KeyError:
-      discard
-    e.timedb.del(tb, key)
-    e.keydb.del(key, tb)
-  else:
-    let d = t - now
-    echo "WAIT BEGIN DURATION ", d, " UNTIL ", t, " NOW IS ", now
-    discard await withTimeout[void](e.trigger[], d.inMilliseconds.int)
-    echo "WAIT END"
-    e.trigger[] = newFuture[void]("expiry" & e.db.name)
-    asyncCheck e.process()
+  echo "REMOVE ", tb, " KEY ", key
+  try:
+    e.db.del(key)
+    echo "NOTIN ", (key notin e.db)
+  except KeyError:
+    discard
+  e.timedb.del(tb, key)
+  e.keydb.del(key, tb)
 
+proc process*(e: Expiry) {.async.} =
+  while true:
+    let now = getTime()
+    while e.timedb.len == 0:
+      discard await withTimeout[void](e.trigger[], e.renew.inMilliseconds.int)
+      e.trigger[] = newFuture[void]("expiry")
+    let tb = e.next
+    let t = tb.fromFlatty(Time)
+    if t <= now:
+      e.remove(tb)
+    else:
+      let d = t - now
+      echo "WAIT BEGIN DURATION ", d, " UNTIL ", t, " NOW IS ", now
+      discard await withTimeout[void](e.trigger[], d.inMilliseconds.int)
+      e.trigger[] = newFuture[void]("expiry")
+      echo "WAIT END"
 
-proc initExpiry(db, timedb, keydb: Database): Expiry =
+proc initExpiry*(db, timedb, keydb: Database): Expiry =
   new(result.trigger)
   result.trigger[] = newFuture[void]("expiry" & db.name)
   result.db = db
@@ -51,7 +50,7 @@ proc initExpiry(db, timedb, keydb: Database): Expiry =
   result.keydb = keydb
   result.renew = initDuration(seconds=3)
 
-proc initExpiry(db: Database, timedbName="", keydbName=""): Expiry =
+proc initExpiry*(db: Database, timedbName="", keydbName=""): Expiry =
   var timedbName = if timedbName == "": "xpt" & db.name else: timedbName
   var keydbName = if keydbName == "": "xpk" & db.name else: keydbName
   initExpiry(
@@ -60,15 +59,18 @@ proc initExpiry(db: Database, timedbName="", keydbName=""): Expiry =
     db.initDatabase(keydbName)
   )
 
-proc expire(e: Expiry, key: string, t: Time) =
+proc expire*(e: Expiry, key: string, t: Time) =
+
+  let retrigger = e.timedb.len == 0 or e.next.fromFlatty(Time) > t
+  
   let tb = t.toFlatty
   e.timedb[tb] = key
   e.keydb[key] = tb
-
-  if e.timedb.len == 0 or e.next.fromFlatty(Time) > t:
+    
+  if retrigger:
     e.trigger[].complete()
 
-template expire(e: Expiry, key: string, d: Duration) =
+template expire*(e: Expiry, key: string, d: Duration) =
   expire(e, key, getTime() + d)
 
 
